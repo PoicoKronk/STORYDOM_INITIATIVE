0dbc2a17313aac8e50775ca8d039dfa95040a8c3
louis nicoletti <louis.nicoletti@gmail.com>
2022-11-29 17:13:03 +0100

    routes remade

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :story, :only [:index, :show, :new, :create, :destroy]
-
-  resources :structure, :only [:new, :create]
+  resources :stories, only: [:index, :show, :new, :create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :character, :place, :item, :only [:new, :create]
+  resources :structures, only: [:new, :create]
+  resources :characters, only: [:new, :create]
+  resources :places, only: [:new, :create]
+  resources :items, only: [:new, :create]
 end
