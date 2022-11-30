bccc992121131a7d9e0b4909f730812fd10f9580
louis nicoletti <louis.nicoletti@gmail.com>
2022-11-30 17:10:28 +0100

    constants added for models

--- app/models/initial.rb
+++ app/models/initial.rb
@@ -1,3 +1,11 @@
 class Initial < ApplicationRecord
   belongs_to :story
+
+  CONTENT = [
+    "In the #{story.place.name}, a #{story.character.name} was looking for a powerful #{story.item.name}.",
+    "In the #{story.place.name}, a #{story.character.name} was looking for a powerful #{story.item.name}.",
+    "In the #{story.place.name}, a #{story.character.name} was flying in the sky. It was guarding a powerful #{story.item.name}.",
+    "In the #{story.place.name}, a #{story.character.name} was flying in the sky. It was guarding a powerful #{story.item.name}."
+  ]
+
 end
--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -1,3 +1,11 @@
 class Trigger < ApplicationRecord
   belongs_to :story
+
+  CONTENT = [
+    "Suddenly, a gust of wind struck the #{story.character.name} . A dragon was flying over #{story.character.gender}.",
+    "Suddenly, a gust of wind struck the #{story.character.name} . A dragon was flying over #{story.character.gender}.",
+    "Suddenly, the #{story.character.name} saw a prince getting closer to the #{story.place.name}.",
+    "Suddenly, the #{story.character.name} saw a prince getting closer to the #{story.place.name}."
+  ]
+
 end
