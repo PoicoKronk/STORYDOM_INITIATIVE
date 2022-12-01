309840e9cbd8f1979631f39069b3e91adcdefc79
Angela Garcia <angela.garcia.pro@outlook.com>
2022-12-01 11:47:37 +0100

    blabla

--- app/models/adventure1.rb
+++ app/models/adventure1.rb
@@ -4,8 +4,8 @@ class Adventure1 < ApplicationRecord
   CONTENTS = [
     "When the #{story.character.name} realized, the dragon was protecting a #{story.item.name} on the top of the #{story.place.name}.",
     "When the #{story.character.name} realized, the dragon was protecting a #{story.item.name} on the top of the #{story.place.name}.",
-    "When #{story.character.name} realized the prince was after its treasure, the dragon was ready to fight back.",
-    "When #{story.character.name} realized the prince was after its treasure, the dragon was ready to fight back."
+    "When the #{story.character.name} realized the prince was after its treasure, the dragon was ready to fight back.",
+    "When the #{story.character.name} realized the prince was after its treasure, the dragon was ready to fight back."
   ]
 
   validates :content, inclusion: { in: CONTENTS }
