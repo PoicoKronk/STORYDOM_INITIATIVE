03ecf9a3eac2dac3b1a30ee60d18f418e2101150
Angela Garcia <angela.garcia.pro@outlook.com>
2022-12-07 17:03:57 +0100

    pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ User.create!(
 )
 
 
-character = Character.create(
+Character.create(
   name: 'Prince',
   typ: 'Human',
   gender: 'He',
@@ -29,8 +29,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979465/Storydom/prince_gyezd9.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Princess',
   typ: 'Human',
   gender: 'She',
@@ -40,8 +39,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979464/Storydom/princess_uabap1.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Knight',
   typ: 'Human',
   gender: 'He',
@@ -51,8 +49,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979464/Storydom/knight_sotcxf.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Hunter',
   typ: 'Human',
   gender: 'Male',
@@ -62,8 +59,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979464/Storydom/hunter_n3hfvm.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Adventurer',
   typ: 'Human',
   gender: 'She',
@@ -73,8 +69,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979464/Storydom/adventurer_rhhoe5.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Pirate',
   typ: 'Human',
   gender: 'He',
@@ -84,8 +79,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670239976/Storydom/pirate2_batrpq.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Cowboy',
   typ: 'Human',
   gender: 'He',
@@ -95,8 +89,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979465/Storydom/cowboy_ejfikj.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Dragon',
   typ: 'Creature',
   gender: 'It',
@@ -106,8 +99,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670327665/Storydom/dragon_p95fcb.png'
 )
 
-
-character = Character.create(
+Character.create(
   name: 'Witch',
   typ: 'Creature',
   gender: 'She',
@@ -117,20 +109,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670240609/Storydom/witch_ggm8xf.png'
 )
 
-
-
-# character = Character.create(
-#   name: 'Ghost',
-#   typ: 'Creature',
-#   gender: 'Male',
-#   pronoun: 'him',
-#   possessive: 'his',
-#   good: true,
-#   photo: 'Ghost.png'
-# )
-
-
-character = Character.create(
+Character.create(
   name: 'Fairy',
   typ: 'Creature',
   gender: 'She',
@@ -140,7 +119,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979464/Storydom/fairy_a2kbfx.png'
 )
 
-character = Character.create(
+Character.create(
   name: 'Elf',
   typ: 'Creature',
   gender: 'He',
@@ -150,7 +129,7 @@ character = Character.create(
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1669979464/Storydom/elf_duhle8.png'
 )
 
-character = Character.create(
+Character.create(
   name: 'Mermaid',
   typ: 'Creature',
   gender: 'She',
@@ -161,229 +140,43 @@ character = Character.create(
 )
 
 
-# character = Character.create(
-#   name: 'Werewolf',
-#   typ: 'Creature',
-#   gender: 'Male',
-#   pronoun: 'him',
-#   possessive: 'his',
-#   good: true,
-#   photo: 'Werewolf.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Vampire',
-#   typ: 'Creature',
-#   gender: 'Male',
-#   pronoun: 'him',
-#   possessive: 'his',
-#   good: true,
-#   photo: 'Vampire.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Dog',
-#   typ: 'Animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Dog.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Cat',
-#   typ: 'Animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Cat.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Bird',
-#   typ: 'Animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Bird.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Lion',
-#   typ: 'Animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Lion.png'
-# )
-
-
-
-# character = Character.create(
-#   name: 'Monkey',
-#   typ: 'Animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Monkey.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Mouse',
-#   typ: 'Animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Mouse.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Rabbit',
-#   typ: 'animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Rabbit.png'
-# )
-
-
-# character = Character.create(
-#   name: 'Daulphin',
-#   typ: 'animal',
-#   gender: 'neutral',
-#   pronoun: 'it',
-#   possessive: 'its',
-#   good: true,
-#   photo: 'Daulphin.png'
-# )
-
-
-# place = Place.create(
-#   name: 'Beach',
-#   typ: 'Nature',
-#   photo: 'Beach.svg'
-# )
-
-
-# place = Place.create(
-#   name: 'Cavern',
-#   typ: 'Nature',
-#   photo: 'Cavern.svg'
-# )
-
-
-# place = Place.create(
-#   name: 'Castle',
-#   typ: 'Building',
-#   photo: 'Castle.svg'
-# )
-
-
-item = Item.create(
+Item.create(
   name: 'Sword',
   typ: 'Weapon',
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670249654/Storydom/short_sword_nzu78v.png'
 )
 
-
-item = Item.create(
+Item.create(
   name: 'Bow',
   typ: 'Weapon',
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670250703/Storydom/bow2_nbqrqn.png'
 )
 
-
-item = Item.create(
+Item.create(
   name: 'Shield',
   typ: 'Weapon',
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670249654/Storydom/shield_izijmz.png'
 )
 
-
-item = Item.create(
+Item.create(
   name: 'Axe',
   typ: 'Weapon',
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670249653/Storydom/axe_p0cm4w.png'
 )
 
-
-item = Item.create(
+Item.create(
   name: 'Wand',
   typ: 'Weapon',
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670250703/Storydom/wand_gvahkr.png'
 )
 
-
-# item = Item.create(
-#   name: 'Whip',
-#   typ: 'Weapon',
-#   photo: 'Whip.png'
-# )
-
-
-# item = Item.create(
-#   name: 'Hat',
-#   typ: 'Clothes',
-#   photo: 'Hat.png'
-# )
-
-
-# item = Item.create(
-#   name: 'Crown',
-#   typ: 'Clothes',
-#   photo: 'Crown.png'
-# )
-
-
-# item = Item.create(
-#   name: 'Cap',
-#   typ: 'Clothes',
-#   photo: 'Cap.png'
-# )
-
-
-# item = Item.create(
-#   name: 'Scarf',
-#   typ: 'Clothes',
-#   photo: 'Scarf.png'
-# )
-
-
-# item = Item.create(
-#   name: 'Glasses',
-#   typ: 'Clothes',
-#   photo: 'Glasses.png'
-# )
-
-
-# item = Item.create(
-#   name: 'Gloves',
-#   typ: 'Clothes',
-#   photo: 'Gloves.png'
-# )
-
-place = Place.create(
+Place.create(
   name: 'Mountain',
   typ: 'Nature',
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670250975/Storydom/Mountain_mlgegz.png'
 )
 
-
-place = Place.create(
+Place.create(
   name: 'Forest',
   typ: 'Nature',
   photo: 'https://res.cloudinary.com/dkaxxgoge/image/upload/v1670250974/Storydom/forest_xmgflb.jpg'
